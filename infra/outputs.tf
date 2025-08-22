a07bbf46fb676e839dff1d5a31572c195f1b29b6
binary-Bug <punitbantia@gmail.com>
2025-08-22 15:04:26 +0530

    update create_infra.yaml and outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -10,43 +10,64 @@ output "app_service_plan_name" {
 	value       = azurerm_service_plan.main.name
 }
 
-# Output the names of all App Services
-output "app_service_names" {
-	description = "Names of all App Services"
-	value       = [for app in azurerm_windows_web_app.apps : app.name]
+output "app_services" {
+  description = "Combined details of all App Services"
+  value = [
+    for app in azurerm_windows_web_app.apps : {
+      name     = app.name
+      url      = app.default_hostname
+      id       = app.id
+    }
+  ]
 }
 
-# Output the default hostnames (URLs) for all App Services
-output "app_service_urls" {
-	description = "Default hostnames (URLs) for all App Services"
-	value       = [for app in azurerm_windows_web_app.apps : app.default_hostname]
+# # Output the names of all App Services
+# output "app_service_names" {
+# 	description = "Names of all App Services"
+# 	value       = [for app in azurerm_windows_web_app.apps : app.name]
+# }
+
+# # Output the default hostnames (URLs) for all App Services
+# output "app_service_urls" {
+# 	description = "Default hostnames (URLs) for all App Services"
+# 	value       = [for app in azurerm_windows_web_app.apps : app.default_hostname]
+# }
+
+# # Output the resource IDs of all App Services
+# output "app_service_ids" {
+# 	description = "Resource IDs of all App Services"
+# 	value       = [for app in azurerm_windows_web_app.apps : app.id]
+# }
+
+output "sql_server_details" {
+  description = "Combined details of the Azure SQL Server"
+  value = {
+    name     = azurerm_mssql_server.main.name
+    fqdn     = azurerm_mssql_server.main.fully_qualified_domain_name
+    username = azurerm_mssql_server.main.administrator_login
+  }
+  sensitive = false
 }
 
-# Output the resource IDs of all App Services
-output "app_service_ids" {
-	description = "Resource IDs of all App Services"
-	value       = [for app in azurerm_windows_web_app.apps : app.id]
-}
+# output "sql_server_name" {
+#   description = "Name of the Azure SQL Server"
+#   value       = azurerm_mssql_server.main.name
+# }
 
-output "sql_server_name" {
-  description = "Name of the Azure SQL Server"
-  value       = azurerm_mssql_server.main.name
-}
+# output "sql_server_fqdn" {
+#   description = "Fully qualified domain name (FQDN) of the SQL Server"
+#   value       = azurerm_mssql_server.main.fully_qualified_domain_name
+# }
 
-output "sql_server_fqdn" {
-  description = "Fully qualified domain name (FQDN) of the SQL Server"
-  value       = azurerm_mssql_server.main.fully_qualified_domain_name
-}
+# output "sql_admin_username" {
+#   description = "SQL Server administrator username"
+#   value       = azurerm_mssql_server.main.administrator_login
+#   sensitive   = false
+# }
 
 output "sql_database_name" {
   description = "Name of the Azure SQL Database"
   value       = azurerm_mssql_database.main.name
-}
-
-output "sql_admin_username" {
-  description = "SQL Server administrator username"
-  value       = azurerm_mssql_server.main.administrator_login
-  sensitive   = false
 }
 
 output "sql_firewall_rule_name" {
